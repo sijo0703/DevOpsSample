46741f5f328ae95255f9a20959a28763c72614d9
Thomas <Sijoy.Thomas@hhs.texas.gov>
2024-11-01 13:39:19 -0500

    Modified test scripts for loading chromedriver

--- tests/test_02_function.py
+++ tests/test_02_function.py
@@ -2,13 +2,16 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 class TestUserManagement(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # Set up Chrome WebDriver
-        cls.driver = webdriver.Chrome()
+        #cls.driver = webdriver.Chrome()
+        cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         cls.driver.get("http://127.0.0.1:5000")
 
     def test_01_add_user(self):
